66f11c2a71f78a0244a5eac8ece61cae65debb67
admike82 <admike82@gmail.com>
2023-07-29 21:23:02 +0200

    add autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "url";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/demineur/",
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
   resolve: {
     alias: [
       {
